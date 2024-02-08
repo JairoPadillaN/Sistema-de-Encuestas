53d8b2be7b318d28ecedf112cc3706c3789e7556
Jairo Padilla Nava <al221910005@gmail.com>
2024-02-08 03:27:18 -0600

    Verificacion de cuenta por email

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -857,7 +857,8 @@
                 @auth
                     <a href="{{ url('/dashboard') }}"
                         class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
-                        Dashboard
+                        <i class="fa-solid fa-house"></i>
+                        Inicio
                     </a>
                 @else
                     <a href="{{ route('login') }}"
